941568610f8ee5d204ae012528e1962f06ca1d4a
Fr3it4z <afonsobaratafreitas@gmail.com>
2024-01-02 23:06:12 +0000

    php commit

---
+++ PHP/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=1, initial-scale=1.0">
+    <title>Site Bacano</title>
+</head>
+<body>
+    <header></header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
