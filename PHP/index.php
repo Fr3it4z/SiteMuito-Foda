5c6eacd9afea262547728895ba5730797f0e6fc0
Freitas <123971995+Fr3it4z@users.noreply.github.com>
2024-01-22 16:21:34 +0000

    fixe3

--- PHP/index.php
+++ PHP/index.php
@@ -5,8 +5,9 @@
     <!-- Porte para o ver no móvel -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="inicial-scale=1, maximum-scale=1">
-    <!-- meta para melhorar a busca para o site-->
+    <!-- titulo na parte de cima do site-->
     <title>Jogo Foda Site</title>
+    <!-- meta para melhorar a busca para o site-->
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -15,16 +16,31 @@
     <!-- fevicon -->
     <link rel="icon" href="">
 </head>
+    <!--inicio do codigo visivel-->
 <body>
+    <!--parte de cima-->
     <header class="cabecalho">
-        <nav>
+        <!--linha de cima para mudar de pagina-->
+        <nav class="firstmenu">
             <a href="">Inicio</a>
             <a href="">Trabalho PAP</a>
             <a href="">Apresentação</a>
             <a href="">Créditos</a>
         </nav>
     </header>
-    <main></main>
+    <!--parte principal-->
+    <main class="conteudo">
+        <!-- parte de cima principal-->
+        <section class="">
+
+            <div class="">
+                <h1 class="titulo">Project L</h1>                
+                <h2 class="subtitulo">"subtitulo"</h2>
+                
+            </div>
+
+        </section>
+    </main>
     <footer></footer>
 </body>
 </html>
