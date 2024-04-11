c565b50ae28c008c1039459c3558ad123799bf9c
Fr3it4z <afonsobaratafreitas@gmail.com>
2024-04-11 01:31:37 +0100

    desing bacano

--- PHP/js/slider.js
+++ PHP/js/slider.js
@@ -3,6 +3,7 @@ const btnPrev = document.getElementById('prev-button');
 const btnNext = document.getElementById('next-button');
 
 let CurrentSlide = 0;
+let time = 0
 
 function hideSlide(){
     slider.forEach(item => item.classList.remove('on'))
@@ -20,6 +21,7 @@ function nextSlide(){
         CurrentSlide++
     }
     showSlide()
+    time = 0;
 }
 
 function prevslide(){
@@ -30,7 +32,19 @@ function prevslide(){
         CurrentSlide--
     }
     showSlide()
+    time = 0;
 }
+
+setInterval(countdown,1000)
+
+function countdown(){
+  time++
+  if(time === 15){
+    nextSlide();
+    time = 0;
+  }
+}
+
 nextSlide
 btnNext.addEventListener('click', nextSlide)
 btnPrev.addEventListener('click', prevslide)
